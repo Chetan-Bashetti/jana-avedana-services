045651efbd62d3302b0ddc698c4c954aafe21e00
chetankb <chetankb619@gmail.com>
2024-06-14 19:01:36 +0800

    Adding Query Model

---
+++ models/queryModel.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const querySchema = new Schema(
+	{
+		user_name: {
+			required: true,
+			type: String
+		},
+		adhar_num: {
+			required: true,
+			type: String
+		},
+		mobile_num: {
+			required: true,
+			type: String
+		},
+		pincode: {
+			required: true,
+			type: String
+		},
+		category: {
+			required: true,
+			type: String
+		},
+		queryDesc: {
+			required: true,
+			type: String
+		},
+		unique_query_id: {
+			required: true,
+			type: String
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+
+const Query = mongoose.model('Query', querySchema);
+
+module.exports = Query;
