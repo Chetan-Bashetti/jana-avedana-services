802e9589fbac2a0f07f9dcf092b81a9a89838d46
chetankb <chetankb619@gmail.com>
2024-06-14 19:01:51 +0800

    Adding Query Routes

---
+++ routes/queryRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+	createNewQuery,
+	getAllQueries,
+	getQueryById,
+	deleteQueryById,
+	getQueryByUniqueId,
+	updateQueryById
+} = require('../controllers/queryControllers');
+
+router.route('/create').post(createNewQuery);
+router.route('/list').get(getAllQueries);
+router.route('/list/unique').get(getQueryByUniqueId);
+router.route('/list/:id').get(getQueryById);
+router.route('/list/delete/:id').delete(deleteQueryById);
+router.route('/list/update/:id').put(updateQueryById);
+
+module.exports = router;
